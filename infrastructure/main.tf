aec6b0dc53c122c333941c3f0e67b36bd2fac777
Joe Darling <joe.darling@cambridgeconsultants.com>
2024-10-31 15:57:29 +0000

    deploy website content to bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,3 +7,16 @@ module "website" {
   cloudfront_aliases = ["${var.domain_prefix}.${var.domain_name}"]
   common_tags        = var.common_tags
 }
+
+resource "aws_s3_object" "web_content" {
+  for_each = fileset("../apps/website", "*")
+  bucket   = module.website.web_content_bucket.id
+  key      = each.value
+  source   = "../apps/website/${each.value}"
+  etag     = filemd5("../apps/website/${each.value}")
+  metadata = {
+    cache-control = each.value == "index.html" ? "no-cache" : ""
+    content-type  = "text/html"
+  }
+
+}
---
+++ infrastructure/modules/website/outputs.tf
@@ -0,0 +1,3 @@
+output "web_content_bucket" {
+  value = aws_s3_bucket.www_bucket
+}
