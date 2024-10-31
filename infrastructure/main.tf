4371cc2134292dad4ca70b127cc6948c5fdff89a
Joe Darling <joe.darling@cambridgeconsultants.com>
2024-10-31 16:41:36 +0000

    Basic React app is deployed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,15 +8,29 @@ module "website" {
   common_tags        = var.common_tags
 }
 
+locals {
+  mime_types = {
+    ".html" = "text/html"
+    ".css"  = "text/css"
+    ".js"   = "application/javascript"
+    ".png"  = "image/png"
+    ".svg"  = "image/svg+xml"
+    ".jpg"  = "image/jpeg"
+    ".gif"  = "image/gif"
+  }
+}
+
 resource "aws_s3_object" "web_content" {
-  for_each = fileset("../apps/website", "*")
-  bucket   = module.website.web_content_bucket.id
-  key      = each.value
-  source   = "../apps/website/${each.value}"
-  etag     = filemd5("../apps/website/${each.value}")
+  for_each     = fileset("../apps/website/dist", "**")
+  bucket       = module.website.web_content_bucket.id
+  key          = each.value
+  source       = "../apps/website/dist/${each.value}"
+  etag         = filemd5("../apps/website/dist/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), "application/octet-stream")
   metadata = {
     cache-control = each.value == "index.html" ? "no-cache" : ""
-    content-type  = "text/html"
+    content_type  = lookup(local.mime_types, regex("\\.[^.]+$", each.value), "application/octet-stream")
+    # content-type  = "text/html"
   }
 
 }
