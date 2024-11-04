2b40dc546675e02423064aec497283544756936a
Joe Darling <joe.darling@cambridgeconsultants.com>
2024-11-04 16:07:11 +0000

    Add navbar

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,19 +1,37 @@
 import barebones from "/barebones.jpeg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import {
+  signIn,
+  fetchAuthSession,
+  confirmSignIn,
+  signOut,
+} from "aws-amplify/auth";
+import NavBar from "./NavBar";
+
 const clientId = import.meta.env.VITE_COGNITO_USERPOOL_CLIENT_ID;
 const cognitoId = import.meta.env.VITE_COGNITO_USERPOOL_ID;
+// const region = import.meta.env.VITE_REGION;
 
-function App() {
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: cognitoId,
+      userPoolClientId: clientId,
+      loginWith: { email: true },
+    },
+  },
+});
+
+const App: React.FC = () => {
   return (
     <>
+      <NavBar />
       <div>
         <img src={barebones} className="logo" alt="Barebones logo" />
       </div>
-      <p>hello</p>
-      {clientId}
-      {cognitoId}
     </>
   );
-}
+};
 
 export default App;
--- apps/website/src/vite-env.d.ts
+++ apps/website/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REGION: string;
+  readonly VITE_COGNITO_USERPOOL_ID: string;
+  readonly VITE_COGNITO_USERPOOL_CLIENT_ID: string;
+  readonly VITE_GRAPHQL_API: string;
+  readonly VITE_API_KEY: string;
+}
