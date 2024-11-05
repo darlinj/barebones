7f76a1119bf699f803c56379ef769e0318782f68
Joe Darling <joe.darling@cambridgeconsultants.com>
2024-11-05 15:31:23 +0000

    First API call working

--- apps/website/src/Home.tsx
+++ apps/website/src/Home.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import barebones from "/barebones.jpeg";
+import { generateClient, GraphQLResult } from "aws-amplify/api";
+
+const client = generateClient();
+
+export const listItems = /* GraphQL */ `
+  query MyQuery {
+    getPublicData(id: "2") {
+      name
+    }
+  }
+`;
 
 const Home: React.FC = () => {
+  const [items, setItems] = useState("");
+
+  useEffect(() => {
+    const apiCall = async () => {
+      try {
+        const result = (await client.graphql({
+          query: listItems,
+        })) as GraphQLResult<{ getPublicData: { name: string } }>;
+
+        setItems(result.data?.getPublicData?.name);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    apiCall();
+  }, []);
   return (
     <div>
       <img src={barebones} className="logo" alt="Barebones logo" />
+
+      <h1>{items}</h1>
     </div>
   );
 };
--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -8,14 +8,24 @@ import { Amplify } from "aws-amplify";
 
 const clientId = import.meta.env.VITE_COGNITO_USERPOOL_CLIENT_ID;
 const cognitoId = import.meta.env.VITE_COGNITO_USERPOOL_ID;
-// const region = import.meta.env.VITE_REGION;
+const region = import.meta.env.VITE_REGION;
+const graphqlEndpoint = import.meta.env.VITE_GRAPHQL_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: cognitoId,
+      identityPoolId: "fooey",
       userPoolClientId: clientId,
       loginWith: { email: true },
+    },
+  },
+  API: {
+    GraphQL: {
+      endpoint: graphqlEndpoint,
+      defaultAuthMode: "apiKey",
+      apiKey: apiKey,
     },
   },
 });
